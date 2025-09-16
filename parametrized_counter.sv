74c42d0e304427e3473487637d755c41b5e7f3fd
Georgios_Kelgiorgis <kelgiorgisg@gmail.com>
2025-09-16 09:18:40 +0300

    Update parametrized_counter.sv

--- parametrized_counter.sv
+++ parametrized_counter.sv
@@ -7,6 +7,7 @@ module counter #(parameter WIDTH = 8) (
     input logic load,
     input logic [WIDTH-1:0] load_value,
     input logic direction
+    output logic done
 );
   
   always @(posedge clk or posedge rst) begin
@@ -26,4 +27,13 @@ module counter #(parameter WIDTH = 8) (
     end
     // If none of the above conditions are met, counter holds its value
 end
+      // "done" flag logic
+  always_comb begin
+    if (!direction && counter == 0)        // reached 0 on down-count
+        done = 1;
+    else if (direction && counter == {WIDTH{1'b1}})  // reached max on up-count
+        done = 1;
+    else
+        done = 0;
+  end
 endmodule
